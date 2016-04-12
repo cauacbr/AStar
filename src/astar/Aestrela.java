381d28f24018a5d82ecef78805c4e0f4d6dd6254
Cauã <Cau@DaimonKratos>
2016-04-12 11:21:21 -0300

    Final

--- src/astar/Aestrela.java
+++ src/astar/Aestrela.java
@@ -17,67 +17,67 @@ import java.util.Map;
 public class Aestrela {
 
     public static void main(String[] args) {
-        Map<String, Map<String, Double>> heuristic = new HashMap<String, Map<String, Double>>();
+        Map<String, Map<String, Double>> heuristica = new HashMap<String, Map<String, Double>>();
         // map for A  
         int h = 2;
-        Map<String, Double> map = new HashMap<String, Double>();
+        Map<String, Double> mapaHeuristica = new HashMap<String, Double>();
 
         if (h == 1) {
             System.out.println("Heuristica 1");
-            map.put("A", 32.0);
-            map.put("B", 19.0);
-            map.put("C", 25.0);
-            map.put("D", 10.0);
-            map.put("E", 8.0);
-            map.put("F", 6.0);
-            map.put("G", 12.0);
-            map.put("H", 0.0);
+            mapaHeuristica.put("A", 32.0);
+            mapaHeuristica.put("B", 19.0);
+            mapaHeuristica.put("C", 25.0);
+            mapaHeuristica.put("D", 10.0);
+            mapaHeuristica.put("E", 8.0);
+            mapaHeuristica.put("F", 6.0);
+            mapaHeuristica.put("G", 12.0);
+            mapaHeuristica.put("H", 0.0);
         }
 
         if (h == 2) {
             System.out.println("Heuristica 2");
-            map.put("A", 45.0);
-            map.put("B", 25.0);
-            map.put("C", 24.0);
-            map.put("D", 13.0);
-            map.put("E", 8.0);
-            map.put("F", 6.0);
-            map.put("G", 12.0);
-            map.put("H", 0.0);
+            mapaHeuristica.put("A", 45.0);
+            mapaHeuristica.put("B", 25.0);
+            mapaHeuristica.put("C", 24.0);
+            mapaHeuristica.put("D", 13.0);
+            mapaHeuristica.put("E", 8.0);
+            mapaHeuristica.put("F", 6.0);
+            mapaHeuristica.put("G", 12.0);
+            mapaHeuristica.put("H", 0.0);
         }
 
-        heuristic.put("A", map);
-        heuristic.put("B", map);
-        heuristic.put("C", map);
-        heuristic.put("D", map);
-        heuristic.put("E", map);
-        heuristic.put("F", map);
-        heuristic.put("G", map);
-        heuristic.put("H", map);
+        heuristica.put("A", mapaHeuristica);
+        heuristica.put("B", mapaHeuristica);
+        heuristica.put("C", mapaHeuristica);
+        heuristica.put("D", mapaHeuristica);
+        heuristica.put("E", mapaHeuristica);
+        heuristica.put("F", mapaHeuristica);
+        heuristica.put("G", mapaHeuristica);
+        heuristica.put("H", mapaHeuristica);
 
-        AmbienteAStar<String> graph = new AmbienteAStar<String>(heuristic);
-        graph.adicionaNo("A");
-        graph.adicionaNo("B");
-        graph.adicionaNo("C");
-        graph.adicionaNo("D");
-        graph.adicionaNo("E");
-        graph.adicionaNo("F");
-        graph.adicionaNo("G");
-        graph.adicionaNo("H");
+        AmbienteAStar<String> grafo = new AmbienteAStar<String>(heuristica);
+        grafo.adicionaNo("A");
+        grafo.adicionaNo("B");
+        grafo.adicionaNo("C");
+        grafo.adicionaNo("D");
+        grafo.adicionaNo("E");
+        grafo.adicionaNo("F");
+        grafo.adicionaNo("G");
+        grafo.adicionaNo("H");
 
-        graph.adicionaFronteira("A", "B", 20);
-        graph.adicionaFronteira("A", "C", 20);
-        graph.adicionaFronteira("B", "D", 10);
-        graph.adicionaFronteira("B", "F", 19);
-        graph.adicionaFronteira("C", "G", 12);
-        graph.adicionaFronteira("C", "H", 25);
-        graph.adicionaFronteira("D", "E", 5);
-        graph.adicionaFronteira("D", "F", 7);
-        graph.adicionaFronteira("E", "H", 8);
-        graph.adicionaFronteira("F", "H", 6);
-        graph.adicionaFronteira("G", "H", 12);
+        grafo.adicionaFronteira("A", "B", 20);
+        grafo.adicionaFronteira("A", "C", 20);
+        grafo.adicionaFronteira("B", "D", 10);
+        grafo.adicionaFronteira("B", "F", 19);
+        grafo.adicionaFronteira("C", "G", 12);
+        grafo.adicionaFronteira("C", "H", 25);
+        grafo.adicionaFronteira("D", "E", 5);
+        grafo.adicionaFronteira("D", "F", 7);
+        grafo.adicionaFronteira("E", "H", 8);
+        grafo.adicionaFronteira("F", "H", 6);
+        grafo.adicionaFronteira("G", "H", 12);
 
-        AgenteAstar<String> aStar = new AgenteAstar<String>(graph);
+        AgenteAstar<String> aEstrela = new AgenteAstar<String>(grafo);
 
         /*Scanner sc = new Scanner(System.in);
         System.out.print("Digite o estado inicial: ");
@@ -88,8 +88,8 @@ public class Aestrela {
         String inicio = "A", fim = "H";
         System.out.println("Estado inicial: " + inicio + "\nEstado Final: " + fim);
 
-        for (String path : aStar.astar(inicio, fim)) {
-            System.out.print(" -> " + path);
+        for (String caminho : aEstrela.astar(inicio, fim)) {
+            System.out.print(" -> " + caminho);
         }
         System.out.println("");
 
